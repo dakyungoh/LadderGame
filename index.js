6b088a595a58c37d0d3a2dfdf3b5615d733597c1
-g <-g>
2022-11-22 02:25:40 +0900

    사다리 인원 입력하기

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-// 1. 사다리 인원 입력하기
-const peoples = +process.argv[2]; // string to number
+// 1. 사다리 인원과 높이 입력하기
+const peoples = +process.argv[2]; //string to number
+const height = +process.argv[3];
+if (peoples < 2 || peoples > 8) {
+  console.log(
+    "peoples의 값이 2명미만이거나 8명을 초과하여 프로그램이 종료됩니다."
+  );
+  process.exit(1);
+}
